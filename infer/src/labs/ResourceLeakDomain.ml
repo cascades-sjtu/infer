626bebc9acdea2e832ef2a33a6b69afe6e58b066
cascades-sjtu <cascades@sjtu.edu.cn>
2023-04-25 15:11:16 +0800

    (3) Integer domain for branching and looping programs

--- infer/src/labs/ResourceLeakDomain.ml
+++ infer/src/labs/ResourceLeakDomain.ml
@@ -8,22 +8,39 @@
 open! IStd
 module F = Format
 
-type t = int
+module FiniteBounds = struct
+  type t = int
 
-let leq ~lhs:_ ~rhs:_ = assert false
+  let leq ~lhs ~rhs = lhs <= rhs
 
-let join _a _b = assert false
+  let join a b = max a b
 
-let widen ~prev:_ ~next:_ ~num_iters:_ = assert false
+  let widen ~prev ~next ~num_iters:_ = join prev next
 
-let pp fmt astate = F.fprintf fmt "%d" astate
+  let pp fmt astate = F.fprintf fmt "%d" astate
+end
 
-let acquire_resource astate = astate + 1
+include AbstractDomain.TopLifted (FiniteBounds)
+open AbstractDomain.Types
 
-let release_resource astate = astate - 1
+let widening_threshold = 5
 
-let has_leak astate = astate > 0
+let widen ~prev ~next ~num_iters =
+  match (prev, next) with
+  | Top, _ | _, Top ->
+      Top
+  | NonTop prev, NonTop next when num_iters < widening_threshold ->
+      NonTop (FiniteBounds.widen ~prev ~next ~num_iters)
+  | NonTop _, NonTop _ ->
+      Top
 
-let initial = 0
+
+let acquire_resource = function Top -> Top | NonTop astate -> NonTop (astate + 1)
+
+let release_resource = function Top -> Top | NonTop astate -> NonTop (astate - 1)
+
+let has_leak = function Top -> false | NonTop astate when astate > 0 -> true | NonTop _ -> false
+
+let initial = NonTop 0
 
 type summary = t
